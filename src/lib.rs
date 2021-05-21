8765d41741cb18eb0fd3161d39a0a55c11c058a5
sharapeco <scin.ichi@gmail.com>
2021-05-21 15:36:42 +0900

    Succeed in calling Math.sin() from Rust.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
 #![no_main]
+#![no_std]
 
-use std::f32;
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+	loop {}
+}
 
-// #[panic_handler]
-// fn panic(_info: &core::panic::PanicInfo) -> ! {
-// 	loop {}
-// }
+// JavaScript の関数をインポート
+#[link(wasm_import_module = "imports")]
+extern {
+	fn math_sin(x: f64) -> f64;
+	fn math_cos(x: f64) -> f64;
+}
 
 #[no_mangle]
 pub fn add(a: i32, b: i32) -> i32 {
@@ -13,6 +19,11 @@ pub fn add(a: i32, b: i32) -> i32 {
 }
 
 #[no_mangle]
-pub fn sin(x: f32) -> f32 {
-	x.sin()
+pub fn sin(x: f64) -> f64 {
+	unsafe { math_sin(x) }
+}
+
+#[no_mangle]
+pub fn cos(x: f64) -> f64 {
+	unsafe { math_cos(x) }
 }
